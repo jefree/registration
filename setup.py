3672129be534d430655b5d086894f4f12138b1b9
jefree <jgarzon920429@gmail.com>
2014-12-23 16:24:48 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='registration',
+    version='0.1.0',
+    packages=['registration',],
+    author='Jefferson Garzon',
+    author_email='jgarzon920429@gmail.com',
+    license='LGPL',
+    url='https://github.com/jefree/registration',
+)
